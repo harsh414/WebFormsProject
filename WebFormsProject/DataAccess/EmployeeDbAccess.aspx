eece0b2a66e8f221db6ecbfa450f9161cbbc0903
harsh.agarwal <harsh.agarwal@IN-9RVTJM3>
2022-07-27 13:36:35 +0530

    updated employeeDbAccess

---
+++ WebFormsProject/DataAccess/EmployeeDbAccess.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EmployeeDbAccess.aspx.cs" Inherits="WebFormsProject.DataAccess.EmployeeDbAccess" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="form-group">
+            <asp:Label ID="Label1" runat="server" Text="EmpNo"></asp:Label>
+            <asp:TextBox ID="txteno" runat="server" CssClass="form-control"
+               ></asp:TextBox>
+            <asp:RequiredFieldValidator ID="deptnorequired" runat="server" ErrorMessage="EmpNo is Required"
+                  ControlToValidate="txteno"></asp:RequiredFieldValidator>
+     
+        </div>
+        <div class="form-group">
+            <asp:Label ID="Label2" runat="server" Text="EmpName"></asp:Label>
+            <asp:TextBox ID="txtename" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                 ErrorMessage="EmpName is Required"
+                 ControlToValidate="txtename"></asp:RequiredFieldValidator>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="Label3" runat="server" Text="Designation"></asp:Label>
+            <asp:TextBox ID="txtdesignation" runat="server" CssClass="form-control"></asp:TextBox>
+             <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+                 ErrorMessage="Designation is Required"
+                 ControlToValidate="txtdesignation"></asp:RequiredFieldValidator>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="Label4" runat="server" Text="Salary"></asp:Label>
+            <asp:TextBox ID="txtsalary" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+
+        <div class="form-group">
+            <asp:Label runat="server">Department</asp:Label>
+             <asp:DropDownList ID="ddlDepart" AutoPostBack="True" OnSelectedIndexChanged="ddlDepart_SelectedIndexChanged" runat="server" CssClass="form-control">
+                
+             </asp:DropDownList>
+        </div>
+
+        <div class="form-group">
+        <asp:Label ID="Label5" runat="server" Text="DeptNo"></asp:Label>
+            <asp:TextBox ReadOnly="true" ID="txtdno" runat="server" CssClass="form-control"
+               ></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="DeptNo is Required"
+                  ControlToValidate="txtdno"></asp:RequiredFieldValidator>
+        </div>
+
+        <div class="form-group">
+            <asp:Button ID="btnNew" runat="server" Text="New" CssClass="btn btn-warning" 
+                OnClick="btnNew_Click"/>
+            <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn btn-success" 
+                OnClick="btnSave_Click"/>
+        </div>
+    </div>
+    <br/>
+    <asp:Label ID="lblstatus" runat="server" Text=""></asp:Label>
+
+    <hr />
+    <asp:GridView ID="gvEmp" runat="server" BackColor="black" 
+    BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" 
+    GridLines="Vertical" AutoGenerateColumns="False" 
+        OnRowCancelingEdit="gvEmp_RowCancelingEdit" OnRowDeleting="gvEmp_RowDeleting" 
+        OnRowEditing="gvEmp_RowEditing" OnRowUpdating="gvEmp_RowUpdating" 
+        OnSelectedIndexChanged="gvEmp_SelectedIndexChanged" CellSpacing="2" 
+         PageSize="2">
+        <AlternatingRowStyle BackColor="#DCDCDC" />
+        <Columns>
+            <asp:BoundField DataField="EmpNo" ReadOnly="true" HeaderText="DeptNo" />
+            <asp:BoundField DataField="EmpName" HeaderText="EmpName" />
+            <asp:BoundField DataField="Designation" HeaderText="Designation" />
+            <asp:BoundField DataField="Salary" HeaderText="Salary" />
+            <asp:BoundField DataField="DeptNo" HeaderText="DeptNo" />
+            <asp:CommandField ButtonType="Button" ShowSelectButton="True" />
+            <asp:CommandField ButtonType="Button" ShowEditButton="True" />
+            <asp:CommandField ButtonType="Button" ShowDeleteButton="True" />
+        </Columns>
+        <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+        <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+        <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#F1F1F1" />
+        <SortedAscendingHeaderStyle BackColor="#0000A9" />
+        <SortedDescendingCellStyle BackColor="#CAC9C9" />
+        <SortedDescendingHeaderStyle BackColor="#000065" />
+</asp:GridView>
+</asp:Content>
---
+++ WebFormsProject/DataAccess/EmployeeDbAccess.aspx.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Application.DataAccess;
+using Application.Entities;
+
+namespace WebFormsProject.DataAccess
+{
+    public partial class EmployeeDbAccess : System.Web.UI.Page
+    {
+        IDataAccess<Employee, int> EmpDbAccess;
+        IDataAccess<Department, int> deptDbAccess;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmpDbAccess = new EmployeeDataAccess();
+            deptDbAccess = new DepartmentDataAccess();
+            if (!IsPostBack)
+            {
+                foreach (var dpt in deptDbAccess.Get())
+                {
+                    ListItem li = new ListItem(dpt.DeptName, dpt.DeptNo.ToString());
+                    ddlDepart.Items.Add(li);
+                }
+                txtdno.Text = ddlDepart.SelectedValue;
+                EmpDbAccess.Get();
+                LoadData();
+            }
+        }
+
+        protected void btnNew_Click(object sender, EventArgs e)
+        {
+            txteno.Text = 0.ToString();
+            txtename.Text = String.Empty;
+            txtdesignation.Text = String.Empty;
+            txtsalary.Text = 0.ToString();
+            txtdno.Text = 0.ToString();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee emp = new Employee()
+                {
+                    EmpNo = Convert.ToInt32(txteno.Text),
+                    EmpName = txtename.Text,
+                    Designation = txtdesignation.Text,
+                    Salary = Convert.ToInt32(txtsalary.Text),
+                    DeptNo = Convert.ToInt32(txtdno.Text)
+                };
+
+                EmpDbAccess.Create(emp);
+                LoadData();
+                lblstatus.Text = "New Employee is Created Successfully...";
+
+            }
+            catch (Exception ex)
+            {
+                lblstatus.Text = ex.Message;
+            }
+        }
+
+        private void LoadData()
+        {
+            gvEmp.DataSource = EmpDbAccess.Get();
+            gvEmp.DataBind();
+        }
+
+        protected void gvEmp_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvEmp.EditIndex = -1;
+            LoadData();
+        }
+
+        protected void gvEmp_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                var eno = Convert.ToInt32(gvEmp.Rows[e.RowIndex].Cells[0].Text);
+                EmpDbAccess.Delete(eno);
+                LoadData();
+            }
+            catch(Exception ex)
+            {
+                lblstatus.Text = ex.Message;
+            }
+        }
+
+        protected void gvEmp_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+
+                Employee emp = new Employee();
+                emp.EmpNo = Convert.ToInt32(gvEmp.Rows[e.RowIndex].Cells[0].Text);
+                emp.EmpName = (gvEmp.Rows[e.RowIndex].Cells[1].Controls[0] as TextBox).Text;
+                emp.Designation = (gvEmp.Rows[e.RowIndex].Cells[2].Controls[0] as TextBox).Text;
+                emp.Salary = Convert.ToInt32((gvEmp.Rows[e.RowIndex].Cells[3].Controls[0] as TextBox).Text);
+                emp.DeptNo = Convert.ToInt32((gvEmp.Rows[e.RowIndex].Cells[4].Controls[0] as TextBox).Text);
+
+                // 2. Update
+                EmpDbAccess.Update(emp.EmpNo, emp);
+                // 3. Remove the Edit effect
+                gvEmp.EditIndex = -1;
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                lblstatus.Text = ex.Message;
+            }
+        }
+
+        protected void gvEmp_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvEmp.EditIndex = e.NewEditIndex;
+
+
+            // read the cells collection for a selected row
+            var cells = gvEmp.Rows[e.NewEditIndex].Cells;
+            // show data in text boxes
+            txteno.Text = cells[0].Text;
+            txtename.Text = cells[1].Text;
+            txtdesignation.Text = cells[2].Text;
+            txtsalary.Text = cells[3].Text;
+            txtdno.Text = cells[4].Text;
+            LoadData();
+        }
+
+        protected void gvEmp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cells = gvEmp.SelectedRow.Cells;
+            txteno.Text = cells[0].Text;
+            txtename.Text = cells[1].Text;
+            txtdesignation.Text = cells[2].Text;
+            txtsalary.Text = cells[3].Text;
+            txtdno.Text = cells[4].Text;
+        }
+
+
+        protected void ddlDepart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtdno.Text= ddlDepart.SelectedValue;
+        }
+    }
+}
